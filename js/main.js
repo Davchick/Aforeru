d0cdcbf82c77f15dac9b07ce62c021721d85cde9
Davut Seyitmammedov <Davchick2000ort@gmail.com>
2023-05-17 18:36:38 +0300

    Footer was created

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const burgerMenu = document.querySelector(".burger__menu");
 const navigationMenu = document.querySelector(".header__nav");
 const background = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 if (burgerMenu) {
   burgerMenu.addEventListener("click", function (e) {
     background.classList.toggle("active");
     burgerMenu.classList.toggle("active");
     navigationMenu.classList.toggle("active");
+    body.classList.toggle("lock");
   });
 }
 
@@ -14,4 +16,5 @@ background.addEventListener("click", function (e) {
   background.classList.toggle("active");
   burgerMenu.classList.toggle("active");
   navigationMenu.classList.toggle("active");
+  body.classList.toggle("lock");
 });
